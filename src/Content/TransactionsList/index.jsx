a373f897c9763be3612973c985f76f455d919f36
Ernesto Mejía <egmm.566@gmail.com>
2019-02-27 23:11:43 +0100

    Showing the 10 smallest expenses

--- src/Content/TransactionsList/index.jsx
+++ src/Content/TransactionsList/index.jsx
@@ -19,14 +19,18 @@ const Transaction = ({ category, date, name, value, currency }) => {
 }
 
 const TransactionsList = ({ data: allData, symbol }) => {
-    const data = allData.sort((a, b) => a.amount.value + b.amount.value);
-    console.log(data);
+    const parseData = (data) => {
+        const expenses = data.filter(d =>
+            d['category_title'].toLowerCase() !== 'income');
+        return expenses.sort((a, b) => parseInt(b.amount.value) - parseInt(a.amount.value));
+    }
+    const data = parseData(allData);
     return (
         <div id="transactions-list">
             <h4 id="list-id">Top 10 smallest expenses</h4>
             <ul id="transactions">
                 {
-                    data.slice(0, 9).map(d => (
+                    data.slice(0, 10).map(d => (
                         <li
                             id={`transaction_${d.description}`}
                             key={`transaction_${d.description}`}
