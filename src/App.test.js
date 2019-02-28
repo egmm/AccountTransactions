26c59b04483a512ca33c3abba705f4df8a33a2eb
Ernesto Mejía <egmm.566@gmail.com>
2019-02-28 20:56:59 +0100

    Tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import SideMenu from './SideMenu';
+import TransactionsList from './Content/TransactionsList';
+import testData from './tests/testData.json';
+import { smalestExpenses } from './tests/smalestExpenses';
+
+Enzyme.configure({ adapter: new Adapter() });
+const symbol = { 'GBP': '£', 'USD': '$', 'EUR': '€' };
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('Checking the side menu', () => {
+  test('that shows the correct username, balance and symbol', () => {
+    const { provider, balance } = testData;
+    const wrapper = mount(
+      <SideMenu
+        provider={provider}
+        balance={balance}
+        symbol={symbol}
+      />
+    );
+    const username = wrapper.find('#username');
+    expect(username.text()).toBe(provider.title);
+
+    const renderedBalance = wrapper.find('#balance>p');
+    expect(renderedBalance.text())
+      .toBe(`${symbol[balance.currency_iso]}${balance.amount}`);
+  });
+
+  describe('Checking the trasnsactions list', () => {
+    test('it is showing the 10 smalest expenses', () => {
+      const { transactions } = testData;
+      const wrapper = mount(
+        <TransactionsList
+          data={transactions}
+          symbol={symbol}
+        />
+      );
+      // Get the transactions once rendered
+      const rendered = wrapper.find('#transactions>li').map(t => {
+        return {
+          category_title: t.find('span.category').text(),
+          description: t.find('span.transaction-description').find('h4').text(),
+          date: t.find('span.transaction-description').find('span.transaction-date').text(),
+          value: t.find('span.value').text()
+        };
+      });
+
+      expect(rendered).toEqual(smalestExpenses);
+    });
+  });
 });
---
+++ src/tests/smalestExpenses.js
@@ -0,0 +1,61 @@
+export const smalestExpenses = [
+    {
+        "date": "2018-07-05",
+        "description": "Transport for London",
+        "category_title": "Transport",
+        "value": '£-2.85',
+    },
+    {
+        "date": "2018-07-04",
+        "description": "Costa Coffee",
+        "category_title": "Eating Out",
+        "value": '£-3.75',
+    },
+    {
+        "date": "2018-07-01",
+        "description": "Wilko",
+        "category_title": "Shopping",
+        "value": '£-10.76',
+    },
+    {
+        "date": "2018-07-03",
+        "description": "Pact Coffee",
+        "category_title": "Shopping",
+        "value": '£-19.99',
+    },
+    {
+        "date": "2018-07-02",
+        "description": "Vodafone",
+        "category_title": "Entertainment",
+        "value": '£-29.99',
+    },
+    {
+        "date": "2018-07-03",
+        "description": "Tesco",
+        "category_title": "Groceries",
+        "value": '£-34.98',
+    },
+    {
+        "date": "2018-07-09",
+        "description": "Pret a Manger",
+        "category_title": "Eating Out",
+        "value": '£-35.87',
+    },
+    {
+        "date": "2018-07-01",
+        "description": "Marks & Spencer",
+        "category_title": "Shopping",
+        "value": '£-45.23',
+    },
+    {
+        "date": "2018-06-29",
+        "description": "British Gas",
+        "category_title": "Bills",
+        "value": '£-48.75',
+    },
+    {
+        "date": "2018-07-01",
+        "description": "Asda",
+        "category_title": "Shopping",
+        "value": '£-51.19',
+    }]
